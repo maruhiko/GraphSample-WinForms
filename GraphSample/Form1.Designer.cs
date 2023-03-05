e73a19ef993028f732e436d3a03b12867a9a24b8
maruhiko <maru_hiko@icould.com>
2023-03-05 18:57:13 +0900

    get Graph

---
+++ GraphSample/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GraphSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(12, 11);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(400, 277);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "拡大";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "縮小";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GraphSample/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot;
+
+namespace GraphSample
+{
+    public partial class Form1 : Form
+    {
+        // データ数
+        // ※データ数は10万（100ミリ秒ごとのデータなら2.7時間分）程度なら問題ないが、100万になると描画が重い
+        static int n = 100;
+
+        // データ(y:データの値、x:時間の想定)
+        double[] y = DataGen.RandomWalk(n);
+        double[] x = Enumerable.Range(0, n).Select(i => (double)i).ToArray();
+
+        // 描画に必要な値
+        double currentTime = 0;  // 現在の時間
+        double delta = 30;       // 現在から+-どれだけの範囲を表示するかの値
+        double minX;             // 現在よりどれだけ過去まで表示するかの値
+        double maxX;             // 現在よりどれだけ未来まで表示するかの値 
+        ScottPlot.Plottable.VLine line;  // 現在に時間を示すバー
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateValues();
+            formsPlot1.Plot.AddScatter(x, y);
+            line = formsPlot1.Plot.AddVerticalLine(currentTime);
+        }
+
+        void UpdateValues()
+        {
+            // 時間の更新(最後のデータで止まるようにする)
+            currentTime = Math.Min(currentTime+1, x.Last());
+            // 描画範囲値の更新
+            minX = currentTime - delta;
+            maxX = currentTime + delta;
+
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            // タイマーにより更新処理（今回は100ミリ秒ごと）
+
+            UpdateValues();
+            // 描画範囲の設定
+            formsPlot1.Plot.SetAxisLimitsX(minX, maxX);
+            // バーの位置の更新
+            line.X = currentTime;
+            // 再描画
+            formsPlot1.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 拡大は描画範囲を狭めることで実現
+            delta = Math.Max(delta - 5, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 縮小は描画範囲を広げることで実現
+            delta += 5;
+        }
+    }
+}
